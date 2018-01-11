b4e100beb825154621a5d78d1b201823c114c9c9
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-01-11 15:39:29 -0200

    First test case.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
+
+  it { expect(@user).to respond_to(:email) }
 end
