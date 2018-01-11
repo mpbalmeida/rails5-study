cd536d83c501bfe16d08c631e06b64973a98aa09
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-01-11 15:43:49 -0200

    Adding more tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe User, type: :model do
   before { @user = FactoryGirl.build(:user) }
 
   it { expect(@user).to respond_to(:email) }
+  it { expect(@user).to respond_to(:name) }
+  it { expect(@user).to respond_to(:password) }
+  it { expect(@user).to respond_to(:password_confirmation) }
 end
