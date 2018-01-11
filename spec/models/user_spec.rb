bb9d38af3f8b1da789c0acb1d1336816763cd0da
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-01-11 19:13:08 -0200

    Changing double quotes to simple quotes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe User, type: :model do
   it {expect(user).to validate_presence_of(:email)}
   it {expect(user).to validate_uniqueness_of(:email).case_insensitive}
   it {expect(user).to validate_confirmation_of(:password)}
-  it {expect(user).to allow_value("patu.duck@gmail.com").for(:email)}
+  it {expect(user).to allow_value('patu.duck@gmail.com').for(:email)}
 
 end
