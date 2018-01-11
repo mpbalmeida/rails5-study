b6034f0e69c8cb680531ac4e7099d916aab4cec9
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-01-11 20:27:20 -0200

    Adding update action to the users controller with tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -63,4 +63,35 @@ RSpec.describe 'Users API', type: :request do
             end
         end
     end
+
+    describe 'PUT /users/:id' do
+        before do
+            headers = {'Accept' => 'application/vnd.taskmanager.v1'}
+            put "/users/#{user_id}", params: {user: user_params }, headers: headers
+        end
+
+        context 'when the request params are valid' do
+            let(:user_params) { { email: 'new@email.com' } }
+            it 'returns status code 200' do
+                expect(response).to have_http_status(200)
+            end
+
+            it 'return the json data for the updated user' do
+                user_response = JSON.parse(response.body, symbolize_names: true)
+                expect(user_response[:email]).to eq(user_params[:email])
+            end
+        end
+
+        context 'when the request params are invalid' do
+            let(:user_params) { { email: 'newinvalidemail@' } }
+            it 'returns status code 422' do
+                expect(response).to have_http_status(422)
+            end
+
+            it 'return the json data for the errors' do
+                user_response = JSON.parse(response.body, symbolize_names: true)
+                expect(user_response).to have_key(:errors)
+            end
+        end
+    end
 end
