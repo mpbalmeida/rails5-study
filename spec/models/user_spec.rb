72697dae26e2686ab45bc38d9572e1330e7fe267
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-01-14 15:05:48 -0200

    Adding association with tasks for model user with tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { build(:user) } 
 
+  it { is_expected.to have_many(:tasks).dependent(:destroy) }
+
   it {expect(user).to validate_presence_of(:email)}
   it {expect(user).to validate_uniqueness_of(:email).case_insensitive}
   it {expect(user).to validate_confirmation_of(:password)}
