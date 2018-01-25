b795c476b426420408aabbe0af6458b5d3e4a5ad
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-01-25 21:43:10 -0200

    Adding new I18n time format

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119224916) do
+ActiveRecord::Schema.define(version: 20180125233124) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
     t.text     "description", limit: 65535
     t.boolean  "done",                      default: false
-    t.date     "deadline"
+    t.datetime "deadline"
     t.integer  "user_id"
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
